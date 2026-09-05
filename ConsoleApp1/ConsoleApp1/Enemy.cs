49020c0b34f8c0f9bd5ac2ddb19903d29f89dcfa
Piero Samaniego <84818862+Piero727@users.noreply.github.com>
2026-09-05 15:57:40 -0500

    jugador, enemigo listo
    Se implemento el script del jugador y el enemigo

---
+++ ConsoleApp1/ConsoleApp1/Enemy.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Enemy
+    {
+        public string Name { get; set; }
+        public int Life {  get; set; }
+        public int Damage { get; set; }
+        public Enemy(string name, int life, int damage)
+        {
+            Name = name;
+            Life = life;
+            Damage = damage;
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Player.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Player
+    {
+        public string Name { get; set; }
+        public int Life { get; set; }
+        public int Damage { get; set; }
+
+        public Player(string name, int life, int damage)
+        {
+            Name = name;
+            Life = life;
+            Damage = damage;
+        }
+    }
+}
